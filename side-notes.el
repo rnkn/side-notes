efed753a79102fdb990928fc4af326b0d88e7dfe
Paul Rankin <hello@paulwrankin.com>
2018-03-11 19:26:16 +1000

    Renamed to side-notes

--- side-notes.el
+++ side-notes.el
@@ -1,4 +1,4 @@
-;;; notes.el --- Easy access to a directory notes file  -*- lexical-binding: t; -*-
+;;; side-notes.el --- Easy access to a directory notes file  -*- lexical-binding: t; -*-
 
 ;; Copyright (c) 2018 Paul W. Rankin
 
@@ -24,16 +24,17 @@
 
 ;;; Code:
 
-(defgroup notes ()
+(defgroup side-notes ()
   "Display a notes file."
   :group 'convenience)
 
-(defcustom notes-hook
+(defcustom side-notes-hook
   nil
   "Hook run after showing notes buffer."
-  :type 'hook)
+  :type 'hook
+  :group 'side-notes)
 
-(defcustom notes-file
+(defcustom side-notes-file
   "notes.txt"
   "Name of the notes file to find.
 
@@ -42,51 +43,49 @@ your project has its own directory and notes file, but if you
 would like to use a file-specific notes file, specify a string
 with `add-file-local-variable'."
   :type 'string
-  :group 'notes)
-(make-variable-buffer-local 'notes-file)
+  :group 'side-notes)
+(make-variable-buffer-local 'side-notes-file)
 
-(defcustom notes-select-window
+(defcustom side-notes-select-window
   t
   "If non-nil, switch to notes window upon displaying it."
   :type 'boolean
   :group 'notes)
 
-(defcustom notes-display-alist
+(defcustom side-notes-display-alist
   '((side . right)
     (window-width . 35)
-    (slot . -1))
+    (slot . 0))
   "Alist used to display notes buffer.
 
 See `display-buffer-in-side-window' for example options."
   :type 'alist
   :group 'notes)
 
-(defvar-local notes-buffer-identify
+(defvar-local side-notes-buffer-identify
   nil
   "Buffer local variable to identify a notes buffer.")
 
-(defun notes-show-or-hide (&optional arg)
+(defun side-notes-toggle-notes (&optional arg)
   "Pop up a window containing notes of current directory.
-If prefixed with ARG, create the `notes-file' if it does not exist."
+If prefixed with ARG, create the `side-notes-file' if it does not exist."
   (interactive)
-  (if notes-buffer-identify
-      (bury-buffer)
+  (if side-notes-buffer-identify
+      (quit-window)
     (let ((display-buffer-mark-dedicated t)
-          (buf (find-file-noselect
-                (expand-file-name notes-file default-directory))))
-      (if (get-buffer-window buf (selected-frame))
-          (delete-windows-on buf (selected-frame))
-        (display-buffer-in-side-window buf notes-display-alist)
-        (with-current-buffer buf
-          (setq notes-buffer-identify t)
-          (run-hooks 'notes-hook))
-        (if notes-select-window
-            (select-window (get-buffer-window buf (selected-frame))))
-        (message "Showing `%s'; %s to hide" buf
+          (buffer (find-file-noselect
+                   (expand-file-name side-notes-file default-directory))))
+      (if (get-buffer-window buffer (selected-frame))
+          (delete-windows-on buffer (selected-frame))
+        (display-buffer-in-side-window buffer side-notes-display-alist)
+        (with-current-buffer buffer
+          (setq side-notes-buffer-identify t)
+          (run-hooks 'side-notes-hook))
+        (if side-notes-select-window
+            (select-window (get-buffer-window buffer (selected-frame))))
+        (message "Showing `%s'; %s to hide" buffer
                  (key-description (where-is-internal this-command
                                                      overriding-local-map t)))))))
 
-(define-key global-map (kbd "M-s n") #'notes-show-or-hide)
-
-(provide 'notes)
-;;; notes.el ends here
+(provide 'side-notes)
+;;; side-notes.el ends here
