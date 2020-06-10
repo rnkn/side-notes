b3469ce3e1edaed80bda04b00a2c48f0457ff35b
Paul W. Rankin <rnkn@rnkn.xyz>
2020-06-10 15:17:58 +1000

    Bump version

--- side-notes.el
+++ side-notes.el
@@ -4,7 +4,7 @@
 
 ;; Author: William Rankin <code@william.bydasein.com>
 ;; Keywords: convenience
-;; Version: 0.3.1
+;; Version: 0.4.0-beta
 ;; Package-Requires: ((emacs "24.5"))
 ;; URL: https://github.com/rnkn/side-notes
 
